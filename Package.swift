de7ac470bfe963d03918cdf804e93e1903c0ddc1
Ravi Tripathi <ravitripathi1996@gmail.com>
2023-06-14 13:49:10 +0530

    Added checksum

--- Package.swift
+++ Package.swift
@@ -34,19 +34,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoogleMaps",
-            url: "https://github.com/ravitripathi/GoogleMapsSPM/releases/download/6.2.1-beta/GoogleMaps.xcframework.zip"
+            url: "https://github.com/ravitripathi/GoogleMapsSPM/releases/download/6.2.1-beta/GoogleMaps.xcframework.zip",
+            checksum: "52cb50db88e4d235109fb8de38ca97e078a0d1cd216245978d555ca537c0b8c4"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
-            url: "https://github.com/ravitripathi/GoogleMapsSPM/releases/download/6.2.1-beta/GoogleMapsBase.xcframework.zip"
+            url: "https://github.com/ravitripathi/GoogleMapsSPM/releases/download/6.2.1-beta/GoogleMapsBase.xcframework.zip",
+            checksum: "27fd35f47736c2d65d494ec70e955b812080c0295c0f023ce1d3db3438fbfa64"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
-            url: "https://github.com/ravitripathi/GoogleMapsSPM/releases/download/6.2.1-beta/GoogleMapsCore.xcframework.zip"
+            url: "https://github.com/ravitripathi/GoogleMapsSPM/releases/download/6.2.1-beta/GoogleMapsCore.xcframework.zip",
+            checksum: "f9fd567a3f995f8498814fb09832cb17ac3e319ea4dfe0fcea7429a371452e3f"
         ),
         .binaryTarget(
             name: "GoogleMapsM4B",
-            url: "https://github.com/ravitripathi/GoogleMapsSPM/releases/download/6.2.1-beta/GoogleMapsM4B.xcframework.zip"
+            url: "https://github.com/ravitripathi/GoogleMapsSPM/releases/download/6.2.1-beta/GoogleMapsM4B.xcframework.zip",
+            checksum: "ae3e0c0b0324c25489d0d0c9f099bd2426ba7bdfcb201162b12ece20fe63e8ed"
         )
     ]
 )
